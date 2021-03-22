33116424062e27a24fd9050c39203c7cea963c45
Hliang <mack_huang@kingdee.com>
2021-03-22 18:09:17 +0800

    feat: 867.转置矩阵-easy

---
+++ 867.转置矩阵.js
@@ -0,0 +1,62 @@
+/*
+ * @lc app=leetcode.cn id=867 lang=javascript
+ *
+ * [867] 转置矩阵
+ *
+ * https://leetcode-cn.com/problems/transpose-matrix/description/
+ *
+ * algorithms
+ * Easy (66.88%)
+ * Likes:    158
+ * Dislikes: 0
+ * Total Accepted:    50.7K
+ * Total Submissions: 74.8K
+ * Testcase Example:  '[[1,2,3],[4,5,6],[7,8,9]]'
+ *
+ * 给你一个二维整数数组 matrix， 返回 matrix 的 转置矩阵 。
+ * 
+ * 矩阵的 转置 是指将矩阵的主对角线翻转，交换矩阵的行索引与列索引。
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：matrix = [[1,2,3],[4,5,6],[7,8,9]]
+ * 输出：[[1,4,7],[2,5,8],[3,6,9]]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：matrix = [[1,2,3],[4,5,6]]
+ * 输出：[[1,4],[2,5],[3,6]]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * m == matrix.length
+ * n == matrix[i].length
+ * 1 
+ * 1 
+ * -10^9 
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} matrix
+ * @return {number[][]}
+ */
+var transpose = function(matrix) {
+  // 十分巧妙的方法 交换矩阵的行索引与列索引
+  return matrix[0].map((y, xIndex) => matrix.map((x, yIndex) => x[xIndex]));
+};
+// @lc code=end
+
